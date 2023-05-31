5e2fa6aa01f37d40a3eabc8be5221dba0fdd2dd6
saratolooti <77777581+saratolooti@users.noreply.github.com>
2023-05-31 19:32:04 +0330

    push files

---
+++ ip-address-tracker/README.md
@@ -0,0 +1,42 @@
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0).
+
+## Table of contents
+
+- [The challenge](#the-challenge)
+- [Screenshot](#screenshot)
+- [Links](#links)
+- [Built with](#built-with)
+- [Author](#author)
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for each page depending on their device's screen size
+- See hover states for all interactive elements on the page
+- See their own IP address on the map on the initial page load
+- Search for any IP addresses or domains and see the key information and location
+
+### Screenshot
+
+![](./screenshot.png)
+
+### Links
+
+- Solution URL: [Add solution URL here]()
+- Live Site URL: [Add live site URL here]()
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- Leaflet.js (for the map)
+- [API](https://geo.ipify.org/)
+
+## Author
+
+- [Frontend Mentor](https://www.frontendmentor.io/profile/saratolooti)
+- [Linkedin](https://www.linkedin.com/in/sara-tolooti-961217212/)
---
+++ ip-address-tracker/src/app.js
@@ -0,0 +1,80 @@
+/* -------------- map setup -------------- */
+var map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
+
+//removing the default zoom btns from map
+document.querySelector(".leaflet-control-zoom").style.display = "none";
+document.querySelector(".leaflet-control-container").style.opacity = "0";
+
+/* -------------- fetch data -------------- */
+let API_Key = "at_W0dfkECAi4i41FkbZpCB8HevhFzaY";
+
+let searchBtn = document.getElementById("submit-btn");
+let searchInp = document.getElementById("search");
+let value = "192.212.174.101";
+
+searchBtn.addEventListener("click", () => {
+  searchOperation();
+});
+
+async function fetchData() {
+  try {
+    const res = await fetch(
+      `https://geo.ipify.org/api/v1?apiKey=${API_Key}&domain=${searchInp.value}`
+    );
+
+    const data = await res.json();
+
+    return data;
+  } catch (error) {
+    console.log("here");
+    showError();
+  }
+}
+
+async function searchOperation() {
+  let result = await fetchData();
+
+  document.getElementById("display-IP").textContent = result.ip;
+  document.getElementById(
+    "display-location"
+  ).textContent = `${result.location.region},\n${result.location.city}`;
+  document.getElementById(
+    "display-timezone"
+  ).textContent = `UTC ${result.location.timezone}`;
+  document.getElementById("display-ISP").textContent = result.isp;
+
+  drawMap(result.location.lat, result.location.lng);
+}
+
+/* -------------- draw map after each search -------------- */
+function drawMap(lat, lng) {
+  let locationIcon = L.icon({
+    iconUrl: "../images/icon-location.svg",
+
+    iconSize: [47, 56.26], // size of the icon
+    iconAnchor: [23, 60], // point of the icon which will correspond to marker's location
+  });
+
+  map.flyTo([lat, lng], 13);
+  L.marker([lat, lng], { icon: locationIcon }).addTo(map);
+}
+
+/* -------------- display whenever error occurs -------------- */
+function showError() {
+  let errorPopup = document.getElementById("popup");
+  errorPopup.style.right = "10px";
+  errorPopup.textContent = "Inserta a valid Domain or IP!";
+
+  setTimeout(() => {
+    errorPopup.style.right = "-500px";
+  }, 3000);
+}
+
+//getting and displaying actaul Ip of user
+searchOperation();
